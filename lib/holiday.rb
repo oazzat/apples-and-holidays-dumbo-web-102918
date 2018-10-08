270dbc249f6b166a22b7d51c962ada128e15b977
Omar A <oazzat19@gmail.com>
2018-10-08 18:23:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  puts (season.to_s).capitalize
+  puts (holiday_hash[:winter].to_s).capitalize
   
 end
 
