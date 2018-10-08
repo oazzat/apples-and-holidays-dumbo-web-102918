4d30fa961402984810b82748898e122fbd9c7991
Omar A <oazzat19@gmail.com>
2018-10-08 19:08:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key,val|
     puts (key.to_s).capitalize + ":"
     val.each do |inkey,inval|
-      puts "  " + (inkey.to_s).capitalize + ": " + inval.join(",")
+      puts "  " + (inkey.to_s).capitalize + ": " + inval.join(", ")
     end
   end
 end
